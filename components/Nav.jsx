5f13a7257a8c8a516d68649528d697508f7d7e39
kushwanth <manukushwanth@gmail.com>
2023-07-23 18:26:40 +0530

    Created the navbar compatable to device

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,7 +7,11 @@ import { useState,useEffect } from "react";
 import  {signin,signout,useSession,getProviders} from "next-auth/react";
 
 const Nav = () => {
-    const isLogged=false;
+    const isLogged=true;
+    const [toggleDropdown, setToggleDropdown] = useState(false);
+    const [providers, setProviders] = useState(null)
+    useEffect(() => {const setProviders=async()=>{const response=await getProviders();setProviders(response);}
+    setProviders();},[])
     return(<nav className="flex-between w-full mb-16 pt-3">
     <Link href="/" className="flex gap-2 flex-center">
         <Image src="/assets/images/logo.svg" alt="Promptshare logo" width={30} height={30} className="object-contain"/>
@@ -15,17 +19,24 @@ const Nav = () => {
     </Link>
     <div className="sm:flex hidden">
     {
-    isLogged?
-    (
-    <div  className="flex gap-3 md:gap-5"><Link href="/create-prompt" className="black_btn">Create post</Link>
-    <button type="button" onClick={signout} className="outline_btn">Signout</button>
-    <Link href='/profile'><Image src="/assets/images/logo.svg" width={37} height={37} className="rounded-full" alt="profile"></Image></Link>
-    </div>
-    )
-    :
-    (<div className="flex gap-3 md:gap-5"></div>)      
+        isLogged?(<div  className="flex gap-3 md:gap-5"><Link href="/create-prompt" className="black_btn">Create post</Link>
+        <button type="button" onClick={signout} className="outline_btn">Signout</button>
+        <Link href='/profile'><Image src="/assets/images/logo.svg" width={37} height={37} className="rounded-full" alt="profile"></Image></Link>
+        </div>)
+        :(<>{providers&&object.values(providers).map((provider)=>(
+        <button type="button" key={provider.name} onClick={()=>signIn(provider.id)} className="black_btn">Sign In</button>))}</>)      
     }
     </div>
-</nav>)}
+    <div className='sm:hidden flex relative'>
+    {
+        isLogged?(<div className='flex'><Image src="/assets/images/logo.svg" width={37} height={37} className='rounded-full'alt='profile' onClick={() => setToggleDropdown(!toggleDropdown)}/>
+        {toggleDropdown && (<div className='dropdown'><Link href='/profile' className='dropdown_link' onClick={() => setToggleDropdown(false)}>My Profile</Link>
+        <Link href='/create-prompt' className='dropdown_link' onClick={() => setToggleDropdown(false)}>Create Prompt</Link>
+        <button type='button' onClick={() => {setToggleDropdown(false); signOut();}} className='mt-5 w-full black_btn'>Sign Out</button></div>)}</div>) 
+        :(<>{providers && Object.values(providers).map((provider) => ( <button type='button' key={provider.name} onClick={() => {signIn(provider.id);}} className='black_btn'>Sign in</button>))}
+        </>)}
+    </div>
+</nav>)
+}
 
 export default Nav
